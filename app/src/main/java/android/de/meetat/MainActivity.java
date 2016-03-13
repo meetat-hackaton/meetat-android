1b69b85a55d5e3b790c4de12a32bd392b92c38bf
rabertol <raina.bertolini@gmail.com>
2016-03-13 07:59:51 +0100

    manisfet permision for ready handy and insert search at bar action

--- app/src/main/java/android/de/meetat/MainActivity.java
+++ app/src/main/java/android/de/meetat/MainActivity.java
@@ -1,5 +1,6 @@
 package android.de.meetat;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.de.meetat.authenticate.LoginFragment;
 import android.de.meetat.authenticate.RegisterFragment;
@@ -15,8 +16,10 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
@@ -84,8 +87,21 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager) MainActivity.this.getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView searchView = null;
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(MainActivity.this.getComponentName()));
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
