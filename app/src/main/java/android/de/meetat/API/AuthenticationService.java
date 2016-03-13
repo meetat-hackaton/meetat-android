792be97c28be222e7e4999767f490a1bd66ac50d
Philipp Daniels <philipp.daniels@gmail.com>
2016-03-13 07:18:45 +0100

    Setup basic ServiceCallback

--- app/src/main/java/android/de/meetat/API/AuthenticationService.java
+++ app/src/main/java/android/de/meetat/API/AuthenticationService.java
@@ -4,5 +4,17 @@ package android.de.meetat.API;
  * Created by mahieke on 12.03.16.
  */
 public interface AuthenticationService {
-    public boolean signup(String nickname, String password);
+
+    /**
+     * @param nickname String
+     * @param email String
+     * @param password String
+     */
+    void signup(String nickname, String email, String password, ServiceCallback<Boolean> callback);
+
+    /**
+     * @param email String
+     * @param password String
+     */
+    void signin(String email, String password, ServiceCallback<Boolean> callback);
 }
--- app/src/main/java/android/de/meetat/API/imp/AuthenticationServiceImp.java
+++ app/src/main/java/android/de/meetat/API/imp/AuthenticationServiceImp.java
@@ -2,21 +2,26 @@ package android.de.meetat.API.imp;
 
 import android.content.Context;
 import android.de.meetat.API.AuthenticationService;
-import android.de.meetat.ServiceCallback;
+import android.de.meetat.API.ServiceCallback;
 
 /**
  * Created by mahieke on 12.03.16.
  */
 public class AuthenticationServiceImp implements AuthenticationService {
-    Context context;
+
+    private Context context;
 
     public AuthenticationServiceImp (Context context) {
         this.context = context;
     }
 
     @Override
-    public boolean signup(String nickname, String email, String password, ServiceCallback<>) {
+    public void signup(String nickname, String email, String password, ServiceCallback<Boolean> callback) {
         // "curl -XPOST http://localhost:8080/users -d 'nickname=philipp&email=someone%40somewhere.com&phone_number=1234&password=lol1234'"
-        return true;
+    }
+
+    @Override
+    public void signin(String email, String password, ServiceCallback<Boolean> callback) {
+
     }
 }
